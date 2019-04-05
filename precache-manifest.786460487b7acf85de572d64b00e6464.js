86491b7d7eb50a7af078b02498f86bb2f094a0c8
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-04-05 15:41:31 -0700

    Updates

---
+++ precache-manifest.786460487b7acf85de572d64b00e6464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wileybb/boo2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee468592e0cf9dc2e29f",
+    "url": "/wileybb/boo2/static/js/runtime~main.c9317cbb.js"
+  },
+  {
+    "revision": "5f9ee1647279f4ff9ff8",
+    "url": "/wileybb/boo2/static/js/main.393473ee.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/wileybb/boo2/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5f9ee1647279f4ff9ff8",
+    "url": "/wileybb/boo2/static/css/main.304d8285.chunk.css"
+  },
+  {
+    "revision": "f125221eecc70db16900b2437e68f0a2",
+    "url": "/wileybb/boo2/index.html"
+  }
+];
